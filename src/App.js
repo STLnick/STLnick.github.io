d91b806cbc7381a0b127cdd7f0ad794e363f9922
Nick Ray <nickrray@gmail.com>
2020-08-13 16:14:25 -0500

    :fire: Remove react router
    Remove react router dependency

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
 import { TopNav } from './components'
 
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <TopNav />
-    </Router>
+    <TopNav />
   )
 }
 
