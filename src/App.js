9274d10d3a4fcbfc2918583839d4347dbe6b2a1b
Nick Ray <nickrray@gmail.com>
2020-08-12 21:25:53 -0500

    :seedling: Setup first component and first page with imports/exports

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+import routes from './routes'
+import { TopNav } from './components'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <TopNav />
+      {routes.map(({ Component, path }, i) => {
+        return (
+          <Route
+            component={Component}
+            exact
+            key={i}
+            path={path}
+          />
+        )
+      })}
+    </Router>
+  )
 }
 
 export default App;
