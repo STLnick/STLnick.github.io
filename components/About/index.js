0179c75c4a9b477ca0410909247b0104e8f89ff1
Nick Ray <nickrray@gmail.com>
2020-08-14 17:59:45 -0500

    :construction: Testing icon import

--- components/About/index.js
+++ components/About/index.js
@@ -1,4 +1,6 @@
-export const About = () => '<h4 id="about">About</h4><div class="test-spacer"></div>'
+import responsiveIcon from '../../img/responsive.svg'
+
 export const About = () => `<section class="section about">
   <h3 class="heading">About</h3>
+  <img src="${responsiveIcon}" />
 </section>`
