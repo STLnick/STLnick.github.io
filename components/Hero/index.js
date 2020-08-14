ad57bacb86260bf348d4f8cd85c6230c6ad392e7
Nick Ray <nickrray@gmail.com>
2020-08-14 17:57:53 -0500

    :speech_balloon: Change hero text content

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -4,8 +4,8 @@ console.log(heroImage)
 
 export const Hero = () => `<div class="hero flex flex--align-center flex--justify-center" style="background-image: url(${heroImage})">
   <div class="hero-text-container flex flex--column flex--align-center flex--justify-center">
-    <h2 class="hero-text">My name is Nick Ray.</h2>
-    <h2 class="hero-text">I'm a JavaScript Web Developer.</h2>
-    <h2 class="hero-text">I 💙 STL.</h2>
+    <h2 class="hero-text">I'm Nick Ray,</h2>
+    <h2 class="hero-text">a JavaScript Web Developer.</h2>
+    <h2 class="hero-text">I 💙 STL and making sites.</h2>
   </div>
 </div > `
