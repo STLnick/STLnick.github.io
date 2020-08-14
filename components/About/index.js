fec6765819d0229ca9fc43196cd68323eb34305f
Nick Ray <nickrray@gmail.com>
2020-08-14 17:59:58 -0500

    :bulb: Comment potential text content of section

--- components/About/index.js
+++ components/About/index.js
@@ -4,3 +4,9 @@ export const About = () => `<section class="section about">
   <h3 class="heading">About</h3>
   <img src="${responsiveIcon}" />
 </section>`
+
+// Nick
+
+// Skills? JS, React, NodeJS
+// I love seeing ideas come to life in design and user experience.
+// I also love Hockey, Biking
