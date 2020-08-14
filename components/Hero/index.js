d4463ab8506b590c7ac25e13113bff88063bb004
Nick Ray <nickrray@gmail.com>
2020-08-14 17:07:42 -0500

    :lipstick: Add text to hero and style it

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -2,6 +2,10 @@ import heroImage from '../../img/archascii.png'
 
 console.log(heroImage)
 
-export const Hero = () => `<div class="hero" style="background-image: url(${heroImage})">
-
+export const Hero = () => `<div class="hero flex flex--align-center flex--justify-center" style="background-image: url(${heroImage})">
+  <div class="hero-text-container flex flex--column flex--align-center flex--justify-center">
+    <h2 class="hero-text">My name is Nick Ray.</h2>
+    <h2 class="hero-text">I'm a JavaScript Web Developer.</h2>
+    <h2 class="hero-text">I 💙 STL.</h2>
+  </div>
 </div > `
