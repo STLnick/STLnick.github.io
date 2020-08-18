b64f1bb38cfdbcb342b4e7e6d600026d00d05a1f
Nick Ray <nickrray@gmail.com>
2020-08-18 00:44:06 -0500

    :sparkles: Show resume on button click, hide on 2nd click or click outside modal

--- index.js
+++ index.js
@@ -17,11 +17,12 @@ ${Blog()}
 ${Contact()}
 ${Footer()}
 `
-
+// Testing scrolling...
 document.querySelector('#about-btn').addEventListener('click', () => {
   document.querySelector('#about').scrollIntoView({ behavior: 'smooth' })
 })
 
+// Change nav svg color on hover
 const navSocialLinks = Array.from(document.querySelectorAll('.nav-social'))
 
 navSocialLinks.forEach(link => {
@@ -36,4 +37,29 @@ navSocialLinks.forEach(link => {
     link.classList.toggle('filter-white')
     link.classList.toggle('filter-secondary')
   })
+})
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const resumeBtn = document.querySelector('.modal-btn')
+const span = document.querySelector('.close')
+
+// Show resume on button click
+resumeBtn.addEventListener('click', () => {
+  modal.style.display = 'block'
+  overlay.style.display = 'block'
+})
+
+// Hide resume on icon click
+span.addEventListener('click', () => {
+  modal.style.display = 'none'
+  overlay.style.display = 'none'
+})
+
+// Hide resume on click outside of modal
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none'
+    overlay.style.display = 'none'
+  }
 })
