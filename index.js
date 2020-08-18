888ec45a4853ae540cbe396c64a8e4aed1982716
Nick Ray <nickrray@gmail.com>
2020-08-17 22:33:55 -0500

    :sparkles: Add listeners to TopNav svgs to change color on hover

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ ${Contact()}
 document.querySelector('#about-btn').addEventListener('click', () => {
   document.querySelector('#about').scrollIntoView({ behavior: 'smooth' })
 })
+
+const navSocialLinks = Array.from(document.querySelectorAll('.nav-social'))
+
+navSocialLinks.forEach(link => {
+  link.addEventListener('mouseover', () => {
+    link.classList.toggle('filter-white')
+    link.classList.toggle('filter-secondary')
+  })
+})
+
+navSocialLinks.forEach(link => {
+  link.addEventListener('mouseleave', () => {
+    link.classList.toggle('filter-white')
+    link.classList.toggle('filter-secondary')
+  })
+})
